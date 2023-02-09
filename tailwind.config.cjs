855d44a0bee5e364d536d4226f630e09228f9b41
rwxdan <rwxdan@gmail.com>
2023-02-09 10:09:08 -0400

    Update tailwind config and switch schema

---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        mono: ['Source Code Pro', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    themes: ['light', 'dark'],
+    darkTheme: 'dark',
+  },
+};
