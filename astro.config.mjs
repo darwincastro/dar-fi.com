4349c208f6e871701296c73d306d05b8f8292ba6
rwxdan <rwxdan@gmail.com>
2023-02-13 13:48:59 -0400

    Use astro-compress
    Rely on this integration to:
    - compress images' sizes.
    - minify html, css and js bundles (even more).
    
    Overall, to optimize the project's size on production.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dar-fi.com',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [compress(), mdx(), sitemap(), tailwind()],
 });
