e53882e7f42e143033cc155adefde59a03818a73
rwxdan <rwxdan@gmail.com>
2023-03-04 12:43:39 -0400

    Add `categories` non-optional value

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,6 +4,7 @@ const blogSchema = z.object({
   title: z.string(),
   description: z.string(),
   tags: z.array(z.string()),
+  categories: z.array(z.string()),
   heroImg: z.string().optional(),
   pubDate: z.coerce.date(),
   updatedDate: z.date().optional(),
