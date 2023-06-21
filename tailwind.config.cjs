af2473535376f44d1335dce4b2dbbbf8d7d147a4
rwxdan <rwxdan@gmail.com>
2023-06-21 00:58:16 -0400

    Create `newLight` and `newDark` colorschemes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,10 +12,34 @@ module.exports = {
   plugins: [require('@tailwindcss/typography'), require('daisyui')],
   daisyui: {
     styled: true,
-    themes: true,
     base: true,
     utils: true,
-    themes: ['light', 'dark'],
-    darkTheme: 'dark',
+    themes: [
+      {
+        newLight: {
+          primary: '#570df8',
+          secondary: '#f000b8',
+          accent: '#1dcdbc',
+          neutral: '#2b3440',
+          'base-100': '#ffffff',
+          info: '#3abff8',
+          success: '#36d399',
+          warning: '#fbbd23',
+          error: '#f87272',
+        },
+        newDark: {
+          primary: '#641ae6',
+          secondary: '#d926a9',
+          accent: '#1fb2a6',
+          neutral: '#2a323c',
+          'base-100': '#1d232a',
+          info: '#3abff8',
+          success: '#36d399',
+          warning: '#fbbd23',
+          error: '#f87272',
+        },
+      },
+    ],
+    darkTheme: 'newDark',
   },
 };
