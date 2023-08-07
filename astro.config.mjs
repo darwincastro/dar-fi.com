1570130fed4ed58d70ddd5de47174a1294038ed6
Daniel Castro <rwxdan@gmail.com>
2023-08-07 11:06:07 -0400

    Remove `astro-compress`
    We're having troubles with these integration
    ever since the maintainer decided to move the package
    to a new account https://github.com/astro-community/astro-compress/issues/163

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dar-fi.com',
   integrations: [
-    compress(),
     mdx({
       syntaxHighlight: 'shiki',
       shikiConfig: {
