afed253cf53918bffc1d9632d080ccf60a7872de
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-15 14:45:11 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,5 +16,6 @@ export default defineConfig({
     }),
     sitemap(),
     tailwind(),
+    compress()
   ],
 });
